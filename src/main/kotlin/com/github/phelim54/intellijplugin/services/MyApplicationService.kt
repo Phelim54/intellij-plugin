b9fcf37c40788e65929fe43ed8b58f4e672c21cc
GitHub Action <action@github.com>
2021-06-28 16:48:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/phelim54/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/phelim54/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.phelim54.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.phelim54.intellijplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/phelim54/intellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/phelim54/intellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.phelim54.intellijplugin.services
 
+import com.github.phelim54.intellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
